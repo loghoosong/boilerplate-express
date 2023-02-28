f574f7c624e02c4c33d6951a49ad8140ff7c33bb
wsl <loghoosong@163.com>
2023-02-28 11:26:10 +0800

    finish challenges

--- myApp.js
+++ myApp.js
@@ -1,39 +1,48 @@
+const bodyParser = require('body-parser');
 let express = require('express');
 let app = express();
+require('dotenv').config();
 
+console.log('Hello Eorld');
 
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.path} - ${req.ip}`);
+    next();
+})
 
+app.use(bodyParser.urlencoded({ extended: false }));
 
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/views/index.html');
+});
 
+app.use('/public', express.static(__dirname + '/public'));
 
+app.get('/json', (req, res) => {
+    if (process.env.MESSAGE_STYLE === 'uppercase') {
+        res.json({ "message": "Hello json".toUpperCase() });
+    } else {
+        res.json({ "message": "Hello json" });
+    }
+});
 
+app.get('/now', (req, res, next) => {
+    req.time = new Date().toString();
+    next();
+}, (req, res) => {
+    res.json({ "time": req.time });
+});
 
+app.get('/:word/echo', (req, res) => {
+    res.json({ "echo": req.params.word });
+});
 
+app.route('/name')
+    .get((req, res) => {
+        res.json({ "name": req.query.first + ' ' + req.query.last });
+    })
+    .post((req, res) => {
+        res.json({ "name": req.body.first + ' ' + req.body.last });
+    });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
